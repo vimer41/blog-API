b6a0cdb5db8babc22854c2bf4a965e3bfd7bc314
Zahar Chernyshev <zaharche05@gmail.com>
2026-06-26 20:44:19 +0300

    feat(databas): add RefreshToken model

---
+++ prisma/migrations/20260626174322_add_refresh_token_model/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "RefreshToken" (
+    "token" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "RefreshToken_pkey" PRIMARY KEY ("token")
+);
+
+-- AddForeignKey
+ALTER TABLE "RefreshToken" ADD CONSTRAINT "RefreshToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
