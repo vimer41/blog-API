ff914474221310735383931cd8c7d455995796ec
Zahar Chernyshev <zaharche05@gmail.com>
2026-06-27 21:53:23 +0300

    docs(swagger): setup swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import { CatchAllHttpExecption } from './common/filters/all-exception.filter.js';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,17 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('ModsenBlog API')
+    .setDescription(
+      'REST API для работы с блогом, возможность создания постов, комментариев, лайки на базе JWT авторизации',
+    )
+    .setVersion('1.0')
+    .addTag('blog')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
