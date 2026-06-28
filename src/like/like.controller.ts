4d0b0b29914345e5012b37ec2853fa583034f2c9
Zahar Chernyshev <zaharche05@gmail.com>
2026-06-28 18:55:18 +0300

    docs: add documentation for like module

--- src/like/like.controller.ts
+++ src/like/like.controller.ts
@@ -1,11 +1,32 @@
 import { Controller, Param, Post, Req, UseGuards } from '@nestjs/common';
 import { LikeService } from './like.service.js';
 import { JwtAuthGuard } from '../auth/guards/jwt.auth.guard.js';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('Лайки')
 @Controller('like')
 export class LikeController {
   constructor(private readonly likeService: LikeService) {}
 
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Поставить/убрать лайк под постом' })
+  @ApiParam({
+    name: 'postId',
+    description: 'uuid поста к которому нужно поставить лайк',
+    example: 'ffed541e-a5fe-4cd3-88de-542c70b07111',
+  })
+  @ApiResponse({ status: 201, description: 'Лайк поставлен или убран' })
+  @ApiResponse({ status: 401, description: 'Требуется авторизация' })
+  @ApiResponse({
+    status: 404,
+    description: 'Пост которому вы пытаетесь поставить лайк не существует',
+  })
   @UseGuards(JwtAuthGuard)
   @Post('post/:postId')
   async likeToPost(@Param('postId') postId: string, @Req() req) {
